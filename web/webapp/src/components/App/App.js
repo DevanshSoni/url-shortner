93598aa8a5511b9c4c26eca5bdf3b9c1133549cc
DevanshSoni <devanshsoni108@gmail.com>
2024-02-25 20:45:14 +0530

    Added url shortner form

--- web/webapp/src/components/App/App.js
+++ web/webapp/src/components/App/App.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <div className="header">
+            <span>URL Shortner</span>
+        </div>
+        <div className="url-submission-form">
+            <form action="">
+                <input type="text" name="" id="" />
+                <button type="button">Shorten</button>
+            </form>
+        </div>
+        <div className="result-section">
+        </div>
+    </>
   )
 }
 
